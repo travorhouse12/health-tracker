c4f609806f506216afc591b0f1d7eddb2722d60e
Travor House <63683715+travorhouse12@users.noreply.github.com>
2024-12-19 04:03:28 +0000

    Theme Coloring

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
 from data.snowflake_data import get_health_data
 from components.summary_card import summary_card
+from styling.css import summary_card_styling, hide_streamlit_menu
 
 st.set_page_config(
     page_title="Travor's Health Tracker",
@@ -14,71 +15,9 @@ st.set_page_config(
 
 st.logo("https://i.ibb.co/0Gt9DcG/Group-1554-1.png", size="large")
 
-hide_streamlit_style = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    .css-10trblm {padding-top: 0px; padding-bottom: 0px;}
-    .css-1d391kg {padding-top: 0px !important;}
-    
-    /* Custom Footer Message */
-    footer:after {
-        content: 'goodbye'; 
-        visibility: visible;
-        display: block;
-        position: relative;
-        padding: 5px;
-        top: 2px;
-        text-align: center;
-        font-size: 14px;
-        color: #ffffff;
-        background-color: #41434A;
-    }
-    </style>
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+hide_streamlit_menu()
 
-# Add custom CSS to remove margin between components
-st.markdown(
-    """
-    <style>
-    .custom-caption.custom {
-        margin-top: -10px
-    }
-    .summary_number.custom {
-        margin-top: -10px;
-        font-size: 54px !important;
-        padding-right: 10px;
-    }
-    .less_than.custom {
-        background: #301D25;
-        display: inline-block;
-        font-size: 20px !important;
-        color: #D44B69;
-        padding: 1px 10px; /* Space around text */
-        border-radius: 5px; /* Rounded corners */
-        margin-top: -5px; /* Ensure this element aligns to the bottom */
-    }
-    .greater_than_or_equal_to.custom {
-        background: #182E25;
-        display: inline-block;
-        font-size: 20px !important;
-        color: #43C171;
-        padding: 1px 10px; /* Space around text */
-        border-radius: 5px; /* Rounded corners */
-        margin-left: -35px; /* Ensure this element aligns to the bottom */
-    }
-    div[data-testid="stColumn"] {
-        background-color: #14161B;
-        padding: 20px;
-        border-radius: 10px;
-        border: 1px solid #292929;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+summary_card_styling()
 
 df = get_health_data()
 
@@ -96,6 +35,7 @@ yesterday_summary_number = df_today['VALUE'].squeeze().astype(int)
 
 difference = yesterday_summary_number - summary_number
 
+st.segmented_control("", ("Day", "Week", "Month"))
 
 cols1, cols2, cols3 = st.columns([1,1, 1])
 
---
+++ styling/css.py
@@ -0,0 +1,68 @@
+import streamlit as st
+
+def summary_card_styling():
+    st.markdown(
+    """
+    <style>
+    .custom-caption.custom {
+        margin-top: -10px
+    }
+    .summary_number.custom {
+        margin-top: -10px;
+        font-size: 54px !important;
+        padding-right: 10px;
+    }
+    .less_than.custom {
+        background: #301D25;
+        display: inline-block;
+        font-size: 20px !important;
+        color: #D44B69;
+        padding: 1px 10px; /* Space around text */
+        border-radius: 5px; /* Rounded corners */
+        margin-top: -5px; /* Ensure this element aligns to the bottom */
+    }
+    .greater_than_or_equal_to.custom {
+        background: #182E25;
+        display: inline-block;
+        font-size: 20px !important;
+        color: #43C171;
+        padding: 1px 10px; /* Space around text */
+        border-radius: 5px; /* Rounded corners */
+        margin-left: -35px; /* Ensure this element aligns to the bottom */
+    }
+    div[data-testid="stColumn"] {
+        background-color: #14161B;
+        padding: 20px;
+        border-radius: 10px;
+        border: 1px solid #292929;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+    
+def hide_streamlit_menu():
+    hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    .css-10trblm {padding-top: 0px; padding-bottom: 0px;}
+    .css-1d391kg {padding-top: 0px !important;}
+    
+    /* Custom Footer Message */
+    footer:after {
+        content: 'goodbye'; 
+        visibility: visible;
+        display: block;
+        position: relative;
+        padding: 5px;
+        top: 2px;
+        text-align: center;
+        font-size: 14px;
+        color: #ffffff;
+        background-color: #41434A;
+    }
+    </style>
+    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
